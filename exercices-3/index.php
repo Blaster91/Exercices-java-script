a7478d3b08d167867876d9eef86c667f6b9a4c6f
Romain Leroy <r.leroy@codeur.online>
2019-10-01 18:25:39 +0200

    test

---
+++ exercices-3/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Bomberman</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <section id="bomberman">
+      <div id="conteneur">
+        <div id="player"></div>
+      </div>
+    </section>
+
+    <script src="js/terrain.js" charset="utf-8"></script>
+    <script src="js/bombe.js" charset="utf-8"></script>
+    <script src="js/player.js" charset="utf-8"></script>
+  </body>
+</html>
