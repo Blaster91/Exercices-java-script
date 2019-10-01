564280c5c6a18536d2c6264663c9ee6756604b84
Romain Leroy <r.leroy@codeur.online>
2019-10-01 19:25:08 +0200

    ajout déplacement dans exercice 3 js

---
+++ exercices-3/js/player.js
@@ -0,0 +1,40 @@
+var playerElt = document.getElementById('player');
+var conteneurElt = document.getElementById('conteneur');
+var allSize = 40;
+var conteneurRect = conteneurElt.getBoundingClientRect();
+var playerRect = playerElt.getBoundingClientRect();
+
+
+// ajout d'un événement qui va récupéré les touches appuyer pour faire bouger le cube
+    document.addEventListener("keydown", function(event){
+
+// si l'événement(qui récupère les valeurs du clavier) est égale à fléche du haut alors éxecute ce code {...}
+          if (event.code == 'ArrowUp') {
+            //si playerRect.top est supérieur a divRect.top alors éxecute ce code {...}
+            if(playerRect.top > conteneurRect.top){
+                playerElt.style.top = (playerElt.offsetTop - allSize) + "px";
+            }
+
+          }
+          else if (event.code == 'ArrowRight') {
+            if(playerRect.right < conteneurRect.right){
+                playerElt.style.left = (playerElt.offsetLeft + allSize) + "px";
+            }
+
+          }
+          else if (event.code == 'ArrowDown') {
+            if(playerRect.bottom < conteneurRect.bottom){
+                playerElt.style.top = (playerElt.offsetTop + allSize)+"px";
+            }
+
+          }
+          else if (event.code == 'ArrowLeft') {
+            if(playerRect.left > conteneurRect.left){
+                playerElt.style.left = (playerElt.offsetLeft - allSize)+"px";
+            }
+          }
+          console.log("conteneurRect = left : " + conteneurRect.left + " top : " + conteneurRect.top + " right : " + conteneurRect.right + " bottom : " + conteneurRect.bottom);
+          console.log("playerRect = left : " + playerRect.left + " top : " + playerRect.top + " right : " + playerRect.right + " bottom : " + playerRect.bottom);
+          conteneurRect = conteneurElt.getBoundingClientRect();
+          playerRect = playerElt.getBoundingClientRect();
+    });
---
+++ exercices-3/js/terrain.js
@@ -0,0 +1,40 @@
+var conteneurElt = document.getElementById('conteneur');
+var sectionElt = document.getElementById('bomberman');
+var largeur = 30;
+var hauteur = 15;
+var allSize = 40;
+var numberWallX = 12;
+var numberWallY = 12;
+var ennemy = [];
+var wall = [];
+
+conteneurElt.style.width = (largeur * 40) + "px";
+conteneurElt.style.height = (hauteur * 40) + "px";
+conteneurElt.style.borderRadius = "5px";
+
+sectionElt.style.display = "flex";
+sectionElt.style.justifyContent = "center";
+
+function creerMur(x, y, cassable){
+    if (!(x == 0 && y == 0) && !(x == largeur*allSize-allSize && y == hauteur*allSize-allSize) && !(x == 0 && y == hauteur*allSize-allSize) && !(x == largeur*allSize-allSize && y == 0)) {
+      var murElt = document.createElement('div');
+      murElt.style.width = allSize + "px";
+      murElt.style.height = allSize + "px";
+      murElt.style.position = "absolute";
+      murElt.style.left = x + "px";
+      murElt.style.top = y + "px";
+
+    if (cassable) murElt.classList.add("mur-cassable"); else murElt.classList.add("mur-incassable");
+      conteneurElt.appendChild(murElt);
+      wall.push(murElt);
+}
+};
+for (var x = 0; x < numberWallX; x++) {
+  for (var y = 0; y < numberWallY; y++) {
+    creerMur(rnd(largeur) * allSize, rnd(hauteur) * allSize, rnd(2)-1);
+  }
+}
+
+function rnd(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
